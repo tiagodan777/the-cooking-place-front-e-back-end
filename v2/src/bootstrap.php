4ecb74efe5f5c70c5b38836f19548d22b6f138c9
Tiago Daniel <tiagoamdaniel488@gmail.com>
2025-03-02 11:24:11 -0100

    ok

---
+++ v2/src/bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+define('APP_ROOT', dirname(__FILE__, 2));
+require_once APP_ROOT . 'config/config.php';
+require_once APP_ROOT . 'src/functions.php';
+
+spl_autoload_register(function($class) {
+    $path = APP_ROOT . 'src/classes/';
+    require $path . $class . '.php';
+});
+
+if (DEV !== true) {
+    set_exception_handler('handle_exception');
+    set_error_handler('handle_error');
+    register_shutdown_function('handle_shutdown');
+}
+
+$cms = new CMS($dsn, $username, $passowrd);
+unset($dsn, $username, $passowrd);
