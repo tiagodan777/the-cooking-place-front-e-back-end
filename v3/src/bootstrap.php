d3ee1fe3155caaf2a6bd970386279f9da9be2b46
Tiago Daniel <tiagoamdaniel488@gmail.com>
2025-03-08 14:59:57 -0100

    instalei as outras dependencias

--- v3/src/bootstrap.php
+++ v3/src/bootstrap.php
@@ -2,11 +2,7 @@
 define('APP_ROOT', dirname(__FILE__, 2));
 require_once APP_ROOT . '/config/config.php';
 require_once APP_ROOT . '/src/functions.php';
-
-spl_autoload_register(function($class) {
-    $path = APP_ROOT . '/src/classes/';
-    require $path . $class . '.php';
-});
+require APP_ROOT . '/vendor/autoload.php';
 
 if (DEV !== false) {
     set_exception_handler('handle_exception');
