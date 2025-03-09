eb0795dc499d246c012b7ecff509bf03b50b1094
Tiago Daniel <tiagoamdaniel488@gmail.com>
2025-03-09 16:55:51 -0100

    mudei tudo o que tinha para mudar, ui super ui

--- v3/src/bootstrap.php
+++ v3/src/bootstrap.php
@@ -16,3 +16,9 @@ unset($dsn, $username, $password);
 //$twig_options['cache'] = APP_ROOT . '/var/cache';
 $twig_options['debug'] = DEV;
 
+$loader = new \Twig\Loader\FilesystemLoader([APP_ROOT . '/templates']);
+$twig = new Twig\Environment($loader, $twig_options);
+$twig->addGlobal('doc_root', DOC_ROOT);
+if (DEV === true) {
+    $twig->addExtension(new \Twig\Extension\DebugExtension);
+}
