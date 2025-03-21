c05e9c711957459aac20beef6d448fd68677d447
Tiago Daniel <tiagoamdaniel488@gmail.com>
2025-03-21 12:38:52 -0100

    verificar se o seo friendly name na URL correspodne ao da database

--- v5/src/pages/article.php
+++ v5/src/pages/article.php
@@ -12,6 +12,10 @@ $ingredientes = explode(',', $receita['ingredientes']);
 $quantidades = explode(',', $receita['quantidades']);
 $passos_preparacao = explode('#', $receita['passos_preparacao']);
 
+if (mb_strtolower($parts[2]) != mb_strtolower($receita['seo_title'])) {
+    redirect(DOC_ROOT . 'article/' . $id . '/' . $receita['seo_title'], [], 301);
+}
+
 $data['receita'] = $receita;
 $data['ingredientes'] = $ingredientes;
 $data['quantidades'] = $quantidades;
--- v5/src/pages/profile.php
+++ v5/src/pages/profile.php
@@ -12,6 +12,10 @@ if (!$membro) {
 
 $receitas = $cms->getArticle()->getAll(member:$id);
 
+if (mb_strtolower($parts[2]) != mb_strtolower($receitas[0]['seo_member'])) {
+    redirect(DOC_ROOT . 'article/' . $id . '/' . $receitas[0]['seo_member'], [], 301);
+}
+
 $data['receitas'] = $receitas;
 $data['membro'] = $membro;
 
