87654632b9199bcdc34d944619039ee4d4a4242b
Tiago Daniel <tiagoamdaniel488@gmail.com>
2025-03-26 22:43:18 -0100

    hoje não avancei muito, mas vou conseguir encontar uma solução

--- v7/src/classes/CMS/Content.php
+++ v7/src/classes/CMS/Content.php
@@ -14,29 +14,26 @@ class Content {
         $arguments['categoria1'] = $category;
         $arguments['membro'] = $member;
         $arguments['membro1'] = $member;
-        $sql = "SELECT
-                c.tipo,
-                r.id, r.titulo, r.descricao, r.data, r.imagem_file, r.membro_id, r.seo_title,
-                CONCAT(m.forename, ' ', m.surname) AS autor,
-                m.picture, m.seo_name AS seo_member,
 
-                q.titulo, q.descricao, q.file, q.receita_acoplada_id, q.membro_id,
-                m.id, CONCAT(m.forename, ' ', m.surname) AS autor, m.picture,
-
-                (SELECT COUNT(conteudo_id)
-                FROM likes
-                WHERE likes.conteudo_id = r.id) AS likes,
-                (SELECT COUNT(conteudo_id)
-                FROM opiniao
-                WHERE opiniao.conteudo_id = r.id) AS opinioes
-
+        $sql = "SELECT c.conteudo_id, c.tipo,
+                CASE
+                    WHEN c.tipo = 'receita' THEN r.id, r.titulo, r.descricao, r.data, r.imagem_file, r.membro_id, r.seo_title,
+                                                CONCAT(m.forename, ' ', m.surname) AS autor,
+                                                m.picture, m.seo_name AS seo_member,
+                                                (SELECT COUNT(conteudo_id)
+                                                FROM likes
+                                                WHERE likes.conteudo_id = r.id) AS likes,
+                                                (SELECT COUNT(conteudo_id)
+                                                FROM opiniao
+                                                WHERE opiniao.conteudo_id = r.id) AS opinioes
+                    WHEN c.tipo = 'quik THEN q.id, q.titulo, q.descricao, q.file, q.receita_acoplada_id, q.membro_id,
+                                            m.id, CONCAT(m.forename, ' ', m.surname) AS autor, m.picture
+                END AS data
                 FROM conteudo AS c
-                JOIN receita AS r ON r.id = c.conteudo_id
-                JOIN membro AS m ON r.membro_id = m.id
-                JOIN quik AS q ON q.id = c.conteudo_id
-                
-                WHERE (r.categoria_id = :categoria OR :categoria1 IS null)
-                AND (r.membro_id = :membro OR :membro1 IS null);";
+                JOIN receita AS r ON r.id = c.id
+                JOIN membro AS m ON m.id = r.membro_id
+                JOIN membro AS m ON m.id = q.membro_id
+                ORDER BY id DESC;";
         return $this->db->runSQL($sql, $arguments)->fetchAll();
     }
 }
