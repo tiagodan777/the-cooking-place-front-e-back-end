a8a3b69cab82e2f5aafa02e7858e6b1139e4b75a
Tiago Daniel <tiagoamdaniel488@gmail.com>
2025-06-05 13:01:25 +0000

    ups, não criei uma brench. mas o que fiz foi o vídeo 3 da serie de websockets adaptado

--- v11/src/classes/CMS/WebSocket.php
+++ v11/src/classes/CMS/WebSocket.php
@@ -10,10 +10,12 @@ use Ratchet\ConnectionInterface;
 
 class WebSocket implements MessageComponentInterface {
     protected $clients;
+    private $pdo;
 
-    public function __construct()
-    {
+
+    public function __construct() {
         $this->clients = new \SplObjectStorage;
+        $this->pdo = new \PDO("mysql:unix_socket=/Applications/MAMP/tmp/mysql/mysql.sock;host=localhost;dbname=the-cooking-place", "root", "root");
     }
 
     public function onOpen(ConnectionInterface $conn) {
@@ -21,20 +23,44 @@ class WebSocket implements MessageComponentInterface {
         echo "New connection! - ({$conn->resourceId})\n";
     }
 
-    public function onMessage(ConnectionInterface $from, $msg)
-    {
-        
+    public function onMessage(ConnectionInterface $from, $msg) {
+        $data = json_decode($msg, true);
+        $action = $data['type'];
+        unset($data['type']);
+
+        switch ($action) {
+            case 'like':
+                echo "<pre>";
+                $sql = "SELECT member_id FROM token
+                        WHERE token = :cookieUser;";
+                $statement = $this->pdo->prepare($sql);
+                $statement->execute(['cookieUser' => $data['cookieUser']]);
+                $memberId = $statement->fetch();
+                var_dump($memberId);
+                echo "</pre>";
+
+                $sql = "INSERT INTO likes (conteudo_id, membro_id)
+                        VALUES (:contentId, :memberId);";
+                $statement = $this->pdo->prepare($sql);
+                $statement->execute(['contentId' => $data['contentId'], 'memberId' => $memberId['member_id']]);
+
+                $this->broadcastNotifications();
+        }
     }
 
-    public function onClose(ConnectionInterface $conn)
-    {
+    public function onClose(ConnectionInterface $conn) {
         $this->clients->detach($conn);
         echo "Conncection {$conn->resourceId} has disconnected\n";
     }
 
-    public function onError(ConnectionInterface $conn, Exception $e)
-    {
+    public function onError(ConnectionInterface $conn, Exception $e) {
         echo "An error ocurred: {$e->getMessage()}\n";
         $conn->close();
+    }
+
+    private function broadcastNotifications() {
+        foreach ($this->clients as $clients) {
+            // Já escrevo
+        }
     }
 }
