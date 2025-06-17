b78a2884d6bb04809dfeeefd3891d7bc668b23c6
Tiago Daniel <tiagoamdaniel488@gmail.com>
2025-06-17 08:58:13 +0000

    ACABEI O MVP!!!

--- v11/src/classes/CMS/Quik.php
+++ v11/src/classes/CMS/Quik.php
@@ -11,7 +11,9 @@ class Quik {
         $this->db = $db;
     }
 
-    public function get($id) {
+    public function get($id, $session = null) {
+        $arguments['session'] = $session;
+
         $sql = "SELECT q.id, q.titulo, q.descricao, q.data, q.receita_acoplada_id, q.youtube_id, q.keywords, q.membro_id, q.seo_title,
                 CONCAT(m.forename, ' ', m.surname) AS autor, m.picture,
                 (SELECT COUNT(conteudo_id) 
@@ -19,15 +21,19 @@ class Quik {
                 WHERE likes.conteudo_id = q.id) AS likes,
                 (SELECT COUNT(conteudo_id)
                 FROM opiniao
-                WHERE opiniao.conteudo_id = q.id) AS opinioes
+                WHERE opiniao.conteudo_id = q.id) AS opinioes,
+                (SELECT COUNT(*)
+                FROM guardado
+                WHERE guardado.conteudo_id = q.id AND guardado.membro_id = :session) AS guardado
                 FROM quik AS q
                 JOIN membro AS m ON m.id = q.membro_id
                 WHERE q.id = :id;";
-        return $this->db->runSQL($sql, [$id])->fetch();
+        return $this->db->runSQL($sql, ['id' => $id, 'session' => $arguments['session']])->fetch();
     }
 
     public function getAll($session = null) {
-        $arguments['session'] = $session;
+        $arguments['session1'] = $session;
+        $arguments['session2'] = $session;
 
         $sql = "SELECT q.id, q.titulo, q.descricao, q.data, q.receita_acoplada_id, q.youtube_id, q.keywords, q.membro_id, q.seo_title, CONCAT(m.forename, ' ', m.surname) AS autor, m.picture,
                 (SELECT COUNT(conteudo_id)
@@ -35,10 +41,13 @@ class Quik {
                 WHERE likes.conteudo_id = q.id) AS likes,
                 (SELECT COUNT(conteudo_id)
                 FROM likes
-                WHERE likes.conteudo_id = q.id AND likes.membro_id = :session) AS i_like,
+                WHERE likes.conteudo_id = q.id AND likes.membro_id = :session1) AS i_like,
                 (SELECT COUNT(conteudo_id)
                 FROM opiniao
-                WHERE opiniao.conteudo_id = q.id) AS opinioes
+                WHERE opiniao.conteudo_id = q.id) AS opinioes,
+                (SELECT COUNT(*)
+                FROM guardado
+                WHERE guardado.conteudo_id = q.id AND guardado.membro_id = :session2) AS guardado
                 FROM quik AS q
                 JOIN membro AS m ON m.id = q.membro_id;";
         return $this->db->runSQL($sql, $arguments)->fetchAll();
